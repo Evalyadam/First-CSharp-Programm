08f4900bd9577e93803bd586ec7f5252c7842e92
Evaly Adam <152657385+Evalyadam@users.noreply.github.com>
2024-01-14 15:57:02 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Enter Username:");
+string Username = Console.ReadLine();
+Console.WriteLine("Enter Password:");
+string Password = Console.ReadLine();
+/*This code asks the user for theyre
+ Username and Password using 'ReadLine' in varaiables.*/
+
+
+Random random = new Random();
+
+int UserID = random.Next(1, 1001);
+
+Console.WriteLine(UserID);
